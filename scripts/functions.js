a6eda1d70beeb0fd693311bc4caefc5e6ea80e9a
Eugene Helland <eugenehelland@mail.com>
2021-11-10 09:52:47 -0700

    Added some css styling with flexbox. Committing changes before modifying js logic.

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,70 +1,60 @@
 function futureEvent() {
-    // Retrieve name of event entered by user
-    var eventName = document.getElementById("feventID").value;
+  // Retrieve name of event entered by user
+  var eventName = document.getElementById("feventID").value;
 
-    return eventName;
-
+  return eventName;
 }
-
 
 function futureDate() {
-    // Retrieve information from form for new event
-    var fDate = {};
-    fDate["date"] = document.getElementById("fdate").value;
-    fDate["time"] = document.getElementById("ftime").value;
+  // Retrieve information from form for new event
+  var fDate = {};
+  fDate["date"] = document.getElementById("fdate").value;
+  fDate["time"] = document.getElementById("ftime").value;
 
-    var localDate = fDate["date"] + ":" + fDate["time"];
-    var eventDate = new Date(localDate).toUTCString();
+  var localDate = fDate["date"] + ":" + fDate["time"];
+  var eventDate = new Date(localDate).toUTCString();
 
-    return eventDate;
-
+  return eventDate;
 }
 
-
 function secondsUntil(date) {
-    // Date object passed from futureDate()
+  // Date object passed from futureDate()
 
-    var eventDate = new Date(date).getTime(); // Convert to milliseconds
+  var eventDate = new Date(date).getTime(); // Convert to milliseconds
 
-    var cdUTC = new Date().toUTCString(); // Convert current date to UTC
-    var currentDate = new Date(cdUTC).getTime();
+  var cdUTC = new Date().toUTCString(); // Convert current date to UTC
+  var currentDate = new Date(cdUTC).getTime();
 
-    // Convert milliseconds to seconds
-    var s1 = eventDate / 1000;
-    var s2 = currentDate / 1000;
+  // Convert milliseconds to seconds
+  var s1 = eventDate / 1000;
+  var s2 = currentDate / 1000;
 
-    var secondsLeft = s1 - s2;
+  var secondsLeft = s1 - s2;
 
-    return secondsLeft;
-
+  return secondsLeft;
 }
 
 function eventCounter(seconds, event, count) {
-    // Takes a count of seconds and outputs days, hours, minutes, seconds
+  // Takes a count of seconds and outputs days, hours, minutes, seconds
 
-    var days = Math.floor(seconds / 86400); // 860400 sec in one day
-    var secs = seconds - (days * 86400);
+  var days = Math.floor(seconds / 86400); // 860400 sec in one day
+  var secs = seconds - days * 86400;
 
-    var hours = Math.floor(secs / 3600);
-    secs = secs - (hours * 3600);
+  var hours = Math.floor(secs / 3600);
+  secs = secs - hours * 3600;
 
-    var mins = Math.floor(secs / 60);
-    secs = secs - (mins * 60);
+  var mins = Math.floor(secs / 60);
+  secs = secs - mins * 60;
 
-    /*var eventPhrase = "<br>" + "<p class='eventPhase'>" + "There are " + days + " days " + hours + " hrs " + mins + " min and " + secs + " secs until " + event + "</p>"; */
+  /*var eventPhrase = "<br>" + "<p class='eventPhase'>" + "There are " + days + " days " + hours + " hrs " + mins + " min and " + secs + " secs until " + event + "</p>"; */
 
-    var eventPhrase = `<br><p id='event${count}'>There are ${days} days, ${hours} hrs, ${mins} mins, and ${secs} seconds until ${event}.`
+  var eventPhrase = `<br><p id='event${count}'>There are ${days} days, ${hours} hrs, ${mins} mins, and ${secs} seconds until ${event}.`;
 
-    return eventPhrase;
-
+  return eventPhrase;
 }
 
 function updateMsg(message) {
-    // Seconds countdown
+  // Seconds countdown
 
-    return document.getElementById("timer").innerHTML = message;
-
+  return (document.getElementById("timer").innerHTML = message);
 }
-
-
-
