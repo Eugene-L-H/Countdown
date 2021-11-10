5c29556302234a70c8edd70990566ef62481b44b
Eugene Helland <eugenehelland@mail.com>
2021-11-10 15:56:45 -0700

    Basic layout, styling and functionality achieved.

--- scripts/functions.js
+++ scripts/functions.js
@@ -48,20 +48,20 @@ function eventCounter(seconds, event) {
 
   /*var eventPhrase = "<br>" + "<p class='eventPhase'>" + "There are " + days + " days " + hours + " hrs " + mins + " min and " + secs + " secs until " + event + "</p>"; */
 
-  let eventPhrase = `<p>There are <b>${days}</b> days, <b>${hours}</b> <i>hrs</i>, <b>${mins}</b> <i>mins</i>, and <b>${secs}</b> <i>seconds</i> until <b><i>${event}</i></b>.</p>`;
+  let eventPhrase = `<p><b>-</b> There are <b>${days}</b> days, <b>${hours}</b> <i>hrs</i>, <b>${mins}</b> <i>mins</i>, and <b>${secs}</b> <i>seconds</i> until <b><i>${event}</i></b>.</p>`;
 
   return eventPhrase;
 }
 
 function addListItem(eventName) {
   let listItem = document.createElement("li");
-  listItem.setAttribute("id", `${eventName}`);
+  listItem.setAttribute("class", `${eventName}`);
   document.getElementById("timers").append(listItem);
 }
 
 function updateMsg(message, eventName) {
   // Seconds countdown
-  return (document.getElementById(eventName).innerHTML = message);
+  return (document.querySelector(`.${eventName}`).innerHTML = message);
 }
 
 class Event {
